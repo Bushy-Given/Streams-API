fc7f9fa31bf8ed404f1240951a93942d237d9138
Bushy <bushy.netshidaulu@gmail.com>
2020-03-20 22:10:37 +0200

    strings with streams

---
+++ src/streams/Run.java
@@ -0,0 +1,23 @@
+package streams;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Bushy-Netshidaulu
+ * on 3/20/2020
+ */
+public class Run {
+
+    public static void main(String[] args) {
+
+        //print the number of times each character apear on a string
+        String word = "bussshyy";
+        Map<Character,Long>  hashMap = new HashMap<>();
+        word.chars().forEach(e -> {
+            long count = word.chars().filter(c -> c == e).count();
+            hashMap.put((char)e, count);
+        });
+        System.out.println(hashMap);
+    }
+}
